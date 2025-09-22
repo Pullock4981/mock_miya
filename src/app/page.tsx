cbca9507c9837ee2507341859b89023000c40a5c
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2025-09-22 12:06:47 +0600

    add all

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground flex justify-center items-center text-5xl font-bold">
-          হে শুরু করেন........
+      হে শুরু করেন........
+      <div className="text-2xl font-medium mt-4">
+        আপনার যাত্রা শুরু হচ্ছে এখানে।
+      </div>
     </div>
   );
 }
