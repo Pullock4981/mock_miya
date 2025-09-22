ec2be30ee3bcdbedd35a8d98a78f076671f55cc8
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2025-09-22 12:02:17 +0600

    add new contain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground flex justify-center items-center text-5xl font-bold">
-          হে শুরু করেন
+          হে শুরু করেন........
     </div>
   );
 }
